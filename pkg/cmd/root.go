f9f25ba8cbe38b39bd3dce6743e09ebdac0015fc
Mohit Singh Panwar <26198379+DragoHex@users.noreply.github.com>
2024-10-21 20:37:38 +0530

    add root command

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -8,17 +9,12 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "github-activity.git",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "gact",
+	Short: "A cli tool to pull recent github activities of a User.",
+	Long: `A cli tool to pull recent github activities of a User.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Pulls latest 10 activities of a user`,
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -31,13 +27,10 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.github-activity.git.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringP("user", "u", "", "GitHub Username")
+	err := rootCmd.MarkPersistentFlagRequired("user")
+	if err != nil {
+		fmt.Printf("error in setting flag as mandatory: %s", err)
+		return
+	}
 }
